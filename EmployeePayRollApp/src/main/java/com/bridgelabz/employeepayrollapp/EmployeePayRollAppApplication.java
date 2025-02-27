81bb8af86123fdfbd6b4a1dd1b1fc6d6d391b8aa
GaganTantwar <panthigagan478@gmail.com>
2025-02-27 13:33:15 +0530

    Gagan Tantwar Extended feat : Database setting as Environment Variable

--- EmployeePayRollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
+++ EmployeePayRollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppApplication.java
@@ -11,6 +11,7 @@ public class EmployeePayRollAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context=SpringApplication.run(EmployeePayRollAppApplication.class,args);
 		log.info("Employee Payroll App Started {} in Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll App DB user is {}",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
